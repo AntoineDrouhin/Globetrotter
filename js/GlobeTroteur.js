36d834013a3d17f891aaef38105ae3d8ac2651cc
Anis Mezdari <mezdari.anis@gmail.com>
2016-02-09 11:43:30 +0100

    CHANGEMENT aNIS Création du JS

---
+++ js/GlobeTroteur.js
@@ -0,0 +1,41 @@
+main() {
+
+// Initialisation des joueurs
+var joueur1 = new array(4);
+var joueur2 = new array(4);
+var joueur3 = new array(4);
+var joueur4 = new array(4);
+
+// initialisation du terrain
+var terrain = [];
+// terrain.push() -- a la fin
+// terrain.pop() -- met à la fin et la supprime
+// terrain.splice(index,0,valeur)
+// terrain.splice(index, 1) : supprime à cette index
+
+// initailisation de la pioche
+var pioche = [];
+
+
+}
+
+remplirPioche(fichier){
+
+}
+
+distributionCarte(joueur){
+
+}
+
+distribuer4Cartes(){
+
+}
+
+insererCarte(position, id){
+
+}
+
+actualiserAffichage(){
+
+}
+
