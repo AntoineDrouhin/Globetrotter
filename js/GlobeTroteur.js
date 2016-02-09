1e3439a60bbd27c3d272a8fab4a3a82ba19cb11f
Antoine Drouhin <antoine.drouhin@gmail.com>
2016-02-09 11:59:25 +0100

    Aurelien ce hero

--- js/GlobeTroteur.js
+++ js/GlobeTroteur.js
@@ -1,12 +1,39 @@
-main() {
+var cCardObject = function(data) {
+    if (data) {
+        this.id = data.id;
+        this.name = data.name;
+        this.surf = data.surf;
+        this.popu = data.popu;
+        this.polu = data.polu;
+        this.pib  = data.pib;
+        this.initHtml();
+    }
+};
+
+cCardObject.prototype.initHtml = function() {
+    this.$el = $("#TEAMPLATE_CARD_HTML").clone();
+    this.$el.find("c-card").attr("id", this.id);
+    this.$el.find(".c-header").html(this.name);
+    this.$el.find(".c-pib").html(this.pib);
+    this.$el.find(".c-polu").html(this.polu);
+    this.$el.find(".c-surf").html(this.surf);
+    this.$el.find(".c-popu").html(this.popu);
+};
+
+cCardObject.prototype.getHtml =  function() {
+    return this.$el.html();
+};
+
+function main() {
+
 
 // Initialisation des joueurs
-
-var joueur1 = new array(4);
-var joueur2 = new array(4);
-var joueur3 = new array(4);
-var joueur4 = new array(4);
+var joueur1 = [4];
+var joueur2 = [4];
+var joueur3 = [4];
+var joueur4 = [4];
 var lesJoueurs = [joueur1,joueur2,joueur3,joueur4];
+
 
 // initialisation du terrain
 var terrain = [];
@@ -21,27 +48,27 @@ var pioche = [];
 
 }
 
-remplirPioche(fichier){
+function remplirPioche(fichier){
 
 }
 
-distributionCarte(joueur){
+function distributionCarte(joueur){
 	joueur.push(pioche[getRandomInt(0,pioche.length + 1]);
 }
 
-distribuer4Cartes(){
-	for(i=0;i<=4;i++){
+function distribuer4Cartes(){
+	for(i=0;i<=LesJoueurs.length;i++){
 		for(j=0;j<=4;j++){
 			distributionCarte(lesJoueurs[i]);
 		}
-	}
-}
-
-insererCarte(position, id){
 
 }
 
-actualiserAffichage(){
+function insererCarte(position, id){
+
+}
+
+function actualiserAffichage(){
 
 }
 
