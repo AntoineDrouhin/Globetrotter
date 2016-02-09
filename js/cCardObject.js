8c9e0ee03c59696556d9edcd75905ff863c6cbf1
Antoine Drouhin <antoine.drouhin@gmail.com>
2016-02-09 16:05:30 +0100

    Réparation des conneries de thibault et puis début de l'assemblage des fonctions dans le main

---
+++ js/cCardObject.js
@@ -0,0 +1,27 @@
+
+var cCardObject = function(data) {
+    if (data) {
+        this.id = data.id;
+        this.name = data.name;
+        this.surf = data.surf;
+        this.popu = data.popu;
+        this.polu = data.polu;
+        this.pib  = data.pib;
+        this.initHtml();
+    }
+};
+
+cCardObject.prototype.initHtml = function() {
+    this.$el = $("#TEAMPLATE_CARD_HTML").clone();
+    this.$el.find("c-card").attr("id", this.id);
+    this.$el.find(".c-header").html(this.name);
+    this.$el.find(".c-pib").html(this.pib);
+    this.$el.find(".c-polu").html(this.polu);
+    this.$el.find(".c-surf").html(this.surf);
+    this.$el.find(".c-popu").html(this.popu);
+};
+
+cCardObject.prototype.getHtml =  function() {
+    return this.$el.html();
+};
+
