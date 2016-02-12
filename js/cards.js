c7de606e5b1749d6b558eb6db8d4dd5fbee72140
Oscar Amzalag <oscaramzalag@aol.com>
2016-02-12 13:51:10 +0100

    drapeau

--- js/cards.js
+++ js/cards.js
@@ -5,7 +5,8 @@
             surf : 8511965,
             popu : 205716890,
             polu : 2.3,
-            pib  : 251800
+            pib  : 251800,
+            link :"https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Flag_of_Brazil.svg/langfr-225px-Flag_of_Brazil.svg.png"
     },
 
     {
@@ -14,7 +15,8 @@
             surf : 70280,
             popu : 472208,
             polu : 9.5,
-            pib  : 221700
+            pib  : 221700,
+            link :"http://www.larousse.fr/encyclopedie/data/images/1009512-Drapeau_de_la_République_dIrlande.jpg"
     },
 
     {
@@ -23,7 +25,8 @@
             surf : 196190,
             popu : 12969606,
             polu : 0.4,
-            pib  : 14700
+            pib  : 14700,
+            link :"https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Flag_of_Senegal.svg/langfr-225px-Flag_of_Senegal.svg.png"
     },
 
     {
@@ -32,7 +35,8 @@
             surf : 131940,
             popu : 10767827,
             polu : 8.7,
-            pib  : 312000
+            pib  : 312000,
+            link :"http://flags.fmcdn.net/data/flags/normal/gr.png"
     },
 
     {
@@ -41,7 +45,8 @@
             surf : 98480,
             popu : 48860500,
             polu : 6.7,
-            pib  : 1164000
+            pib  : 1164000,
+            link :"https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Flag_of_South_Korea.svg/langfr-225px-Flag_of_South_Korea.svg.png"
     },
 
     {
@@ -50,7 +55,8 @@
             surf : 475440,
             popu : 20129878,
             polu : 0.4,
-            pib  : 25800
+            pib  : 25800,
+            link :"https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Flag_of_Cameroon.svg/langfr-225px-Flag_of_Cameroon.svg.png"
     },
 
     {
@@ -59,7 +65,8 @@
             surf : 41290,
             popu : 8014000,
             polu : 4.7,
-            pib  : 665900
+            pib  : 665900,
+            link :"https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Flag_of_Switzerland.svg/langfr-225px-Flag_of_Switzerland.svg.png"
     },
 
     {
@@ -68,7 +75,8 @@
             surf : 446550,
             popu : 32309239,
             polu : 1.7,
-            pib  : 101800
+            pib  : 101800,
+            link :"http://www.drapeauxdespays.fr/data/flags/ultra/ma.png"
     },
 
     {
@@ -77,7 +85,8 @@
             surf : 237500,
             popu : 21848504,
             polu : 3.9,
-            pib  : 185300
+            pib  : 185300,
+            link :"https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Flag_of_Romania.svg/langfr-225px-Flag_of_Romania.svg.png"
     },
 
     {
@@ -86,7 +95,8 @@
             surf : 8511965,
             popu : 29890686,
             polu : 0.1,
-            pib  : 18300
+            pib  : 18300,
+            link :"http://medias.doublet.pro/medias/images/produits/874b2958c14a5f79b23f018f21c37c60.jpg"
     },
 
     {
@@ -95,7 +105,8 @@
             surf : 10400,
             popu : 4140289,
             polu : 4.7,
-            pib  : 41500
+            pib  : 41500,
+            link :"https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Flag_of_Lebanon.svg/langfr-225px-Flag_of_Lebanon.svg.png"
     },
 
     {
@@ -104,7 +115,8 @@
             surf : 514000,
             popu : 67091089,
             polu : 4.1,
-            pib  : 345600
+            pib  : 345600,
+            link :"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Flag_of_Thailand.svg/langfr-225px-Flag_of_Thailand.svg.png"
     },
 
     {
@@ -113,7 +125,8 @@
             surf : 1219912,
             popu : 50586757,
             polu : 9.7,
-            pib  : 422000
+            pib  : 422000,
+            link :"https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Flag_of_South_Africa.svg/langfr-225px-Flag_of_South_Africa.svg.png"
     },
 
     {
@@ -122,7 +135,8 @@
             surf : 547030,
             popu : 65800000,
             polu : 5.6,
-            pib  : 2808000
+            pib  : 2808000,
+            link :"http://www.codepostalpro.com/images/france-drapeau.png"
     },
 
     {
@@ -131,7 +145,8 @@
             surf : 9984670,
             popu : 34300083,
             polu : 15.6,
-            pib  : 1759000
+            pib  : 1759000,
+            link :"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Flag_of_Canada_(Pantone).svg/langfr-225px-Flag_of_Canada_(Pantone).svg.png"
     },
 
     {
@@ -140,7 +155,8 @@
             surf : 2381740,
             popu : 35406303,
             polu : 3.3,
-            pib  : 183400
+            pib  : 183400,
+            link :"http://www.drapeau-dz.com/sites/default/files/drapeau-algerien.jpg"
     },
 
     {
@@ -149,7 +165,8 @@
             surf : 268680,
             popu : 4327944,
             polu : 7.3,
-            pib  : 168800
+            pib  : 168800,
+            link :"http://media.20minutes.fr/2015/2000px-flag_of_new_zealandsvg.png"
     },
 
     {
@@ -158,7 +175,8 @@
             surf : 1138910,
             popu : 45239079,
             polu : 1.6,
-            pib  : 321500
+            pib  : 321500,
+            link :"https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Flag_of_Colombia.svg/langfr-225px-Flag_of_Colombia.svg.png"
     },
 
     {
@@ -167,7 +185,8 @@
             surf : 3287590,
             popu : 1205073612,
             polu : 1.8,
-            pib  : 1843000
+            pib  : 1843000,
+            link :"http://www.drapeauxdespays.fr/data/flags/big/in.png"
     },
 
     {
@@ -176,7 +195,8 @@
             surf : 377835,
             popu : 127368088,
             polu : 9.3,
-            pib  : 5855000
+            pib  : 5855000,
+            link :"http://vignette4.wikia.nocookie.net/desencyclopedie/images/f/fb/Japon_flag.gif/revision/20070503060438"
     },
 
     {
@@ -185,7 +205,8 @@
             surf : 301230,
             popu : 61261254,
             polu : 6.7,
-            pib  : 2246000
+            pib  : 2246000,
+            link :"http://www.dinosoria.com/pays/drapeaux/italie.jpg"
     },
 
     {
@@ -194,7 +215,8 @@
             surf : 43094,
             popu : 5543453,
             polu : 5.9,
-            pib  : 330500
+            pib  : 330500,
+            link :"http://i.skyrock.net/4191/11154191/pics/446124635.gif"
     },
 
     {
@@ -203,7 +225,8 @@
             surf : 2780400,
             popu : 42192494,
             polu : 5.4,
-            pib  : 435200
+            pib  : 435200,
+            link :"https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Flag_of_Argentina.svg/langfr-225px-Flag_of_Argentina.svg.png"
     },
 
     {
@@ -212,7 +235,8 @@
             surf : 1001450,
             popu : 83688164,
             polu : 3,
-            pib  : 231900
+            pib  : 231900,
+            link :"http://www.varinard.com/sites/varinard.com/files/eg_dp.gif"
     },
     {
             id   :  25,
@@ -220,7 +244,8 @@
             surf : 357021,
             popu : 81305856,
             polu : 9.1,
-            pib  : 3629000
+            pib  : 3629000,
+            link :"https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Flag_of_Germany.svg/langfr-225px-Flag_of_Germany.svg.png"
     },
 
     {
@@ -229,7 +254,8 @@
             surf : 1972550,
             popu : 114975406,
             polu : 4,
-            pib  : 185000
+            pib  : 185000,
+            link :"http://www.larousse.fr/encyclopedie/data/images/1009548-Drapeau_du_Mexique.jpg"
     },
 
     {
@@ -238,7 +264,8 @@
             surf : 163610,
             popu : 10732900,
             polu : 2.6,
-            pib  : 48900
+            pib  : 48900,
+            link :"http://www.larousse.fr/encyclopedie/data/images/1009611-Drapeau_de_la_Tunisie.jpg"
     },
 
     {
@@ -247,7 +274,8 @@
             surf : 41526,
             popu : 16730632,
             polu : 11.2,
-            pib  : 858300
+            pib  : 858300,
+            link :"http://cdn.deguisetoi.fr/images/rep_art/gra/217/9/217971/drapeau-pays-bas_217971.jpg"
     },
 
     {
@@ -256,7 +284,8 @@
             surf : 7886650,
             popu : 22015576,
             polu : 19,
-            pib  : 1507000
+            pib  : 1507000,
+            link :"http://www.guide-australie.fr/wp-content/uploads/2014/02/drapeau-australie.jpg"
     },
 
     {
@@ -265,7 +294,8 @@
             surf : 56542,
             popu : 4480043,
             polu : 5.4,
-            pib  : 608000
+            pib  : 608000,
+            link :"http://flags.fmcdn.net/data/flags/normal/hr.png"
     },
 
     {
@@ -274,16 +304,8 @@
             surf : 20770,
             popu : 7590758,
             polu : 9.2,
-            pib  : 245300
-    },
-
-    {
-            id : 32,
-            name : "Canada",
-            surf : 9984670,
-            popu : 34300083,
-            polu : 15.6,
-            pib  : 1759000
+            pib  : 245300,
+            link :"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Flag_of_Israel.svg/langfr-225px-Flag_of_Israel.svg.png"
     },
 
     {
@@ -292,7 +314,8 @@
             surf : 92391,
             popu : 10781459,
             polu : 5,
-            pib  : 241900
+            pib  : 241900,
+            link :"http://www.portugalmania.com/images/drapeau-portugalG.gif"
     },
 
     {
@@ -301,7 +324,8 @@
             surf : 17098178,
             popu : 138082178,
             polu : 12.5,
-            pib  : 1291000
+            pib  : 1291000,
+            link :"http://flags.fmcdn.net/data/flags/normal/ru.png"
     },
 
     {
@@ -310,7 +334,8 @@
             surf : 30528,
             popu : 10438453,
             polu : 9.2,
-            pib  : 529000
+            pib  : 529000,
+            link :"http://i1.wp.com/jeretiens.net/wp-content/uploads/2014/07/Drapeau_belge_belgique2.gif"
     }
 ];
 
