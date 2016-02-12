2dc044963fd496c3b8682f45a2f69de68519e923
BThibaut <tibo.blairon@gmail.com>
2016-02-12 14:45:25 +0100

    Ajout des drapeaux dans les Header des cartes

--- js/cCardObject.js
+++ js/cCardObject.js
@@ -18,6 +18,8 @@ cCardObject.prototype.initHtml = function() {
     this.$el = $("#TEAMPLATE_CARD_HTML").clone();
     this.$el.find(".c-card").attr("id", "carte-" + this.id);
     this.$el.find(".c-header").html(this.name);
+    this.$el.find(".c-header").css({"background-image": "url(" + this.link + ")", "background-size": "cover"});
+    
     this.$el.find(".c-pib").html(splitNumber(this.pib));
     this.$el.find(".c-polu").html(this.polu);
     this.$el.find(".c-surf").html(splitNumber(this.surf));
